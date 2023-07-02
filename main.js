2417db644071d5bcbe3646f712154793c53da157
doquangvnla <138232649+doquangvnla@users.noreply.github.com>
2023-07-02 09:00:33 +0700

    1

---
+++ main.js
@@ -0,0 +1,162 @@
+//const socket = io('http://localhost:3000');
+//const socket = io('http://localhost:3000', { transports: ['websocket', 'polling', 'flashsocket'] });
+const socket = io('https://socketio-xksy.onrender.com', { transports: ['websocket', 'polling', 'flashsocket'] });
+const str = openStream();
+
+$('#div-chat').hide();
+
+
+socket.on('DANH_SACH', arrUser =>{
+	
+	console.log(arrUser);
+	arrUser.forEach(user => {
+        const { ten, peerId } = user;
+        $('#ulUser').append(`<div class="participant-item">
+          <div class="participant-avatar"></div>
+          <div class="participant-name">${ten}</div>
+        </div>`);
+		
+		//them video nguoi den truoc
+		var id = $('#my-peer').html();
+		if(id != peerId){
+			$('#remote_video').append(`<video class="video-frame" id="remoteStream${peerId}" controls></video>`);
+		}
+		
+    });
+	
+	socket.on('CO_NGUOI', user =>{
+		console.log(user);
+		const { ten, peerId } = user;
+		$('#ulUser').append(`<div class="participant-item" id="${peerId}">
+          <div class="participant-avatar"></div>
+          <div class="participant-name">${ten}</div>
+        </div>`);
+		
+		//them video cho nguoi moi
+        $('#remote_video').append(`<video class="video-frame" id="remoteStream${peerId}" controls></video>`);
+		
+		//goi voi nguoi moi vao
+		str
+		.then(stream => {
+			const call = peer.call(peerId, stream);
+			call.on('stream', remoteStream => playStream('remoteStream'+peerId, remoteStream));
+		});
+	});	
+		
+	socket.on('AI_DO_NGAT_KET_NOI', peerId => {
+        $(`#${peerId}`).remove();
+		$(`#remoteStream${peerId}`).remove();
+    });
+	
+});
+
+//chat
+socket.on('chat message', (msg) => {
+	const form = document.getElementById('chat-form');
+	const input = document.getElementById('chat-input');
+	const messages = document.getElementById('messages');
+	
+	var strArr = msg.split(":");
+	var li = document.createElement('li');
+	var li_ten = document.createElement('span');
+	var li_chu = document.createElement('span');
+	
+	var username = $('#txtUsername').val();
+	if(username == strArr[0]){
+		li_ten.style.color = "red";
+	}
+	
+	li_ten.textContent = strArr[0] + ": ";
+	li_chu.textContent = strArr[1];
+	li.appendChild(li_ten);
+	li.appendChild(li_chu);
+	messages.appendChild(li);
+});
+
+
+socket.on('DANG_KY_THAT_BAT', () => alert('Vui long chon username khac!'));
+
+function openStream() {
+    const config = { audio: false, video: true };
+    return navigator.mediaDevices.getUserMedia(config);
+}
+
+function playStream(idVideoTag, stream) {
+    const video = document.getElementById(idVideoTag);
+	if(idVideoTag == 'localStream'){
+		video.muted = true;
+	}
+    video.srcObject = stream;
+    video.play();
+}
+
+//openStream()
+//.then(stream => playStream('localStream', stream));
+
+var peer = new Peer();
+
+peer.on('open', function(id) {
+	$('#my-peer').append(id);
+	
+	
+	$('#btnSignUp').click(() => {
+		const username = $('#txtUsername').val();
+		if(socket.connected == false){
+			alert('Connect socket fail!!');
+			return;
+		}
+		socket.emit('NGUOI_DUNG',{ten : username, peerId : id});
+		
+		str.then(stream => {
+			$('#div-chat').show();
+			$('#div-dang-ky').hide();
+			playStream('localStream', stream);
+		}).catch(err => {
+			$('#div-chat').hide();
+			$('#div-dang-ky').show();
+			alert('permission denied!!')
+		});;
+	});
+  });
+  
+
+//Callee
+peer.on('call', call => {
+    str
+    .then(stream => {
+        call.answer(stream);
+        playStream('localStream', stream);
+        call.on('stream', remoteStream => playStream('remoteStream'+call.peer, remoteStream));
+    });
+});
+
+$('#ulUser').on('click', 'li', function() {
+    const id = $(this).attr('id');
+    console.log(id);
+    openStream()
+    .then(stream => {
+        playStream('localStream', stream);
+        const call = peer.call(id, stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    });
+});
+
+//chat
+$('#send_msg').on('click', function(event) {
+	event.preventDefault();
+    const input = document.getElementById('chat-input');
+	if (input.value) {
+		var username = $('#txtUsername').val();
+        socket.emit('chat message', username + ":" + input.value);
+        input.value = '';
+    }
+});
+
+function toggleChat() {
+  var chatBox = document.getElementById("chatBox");
+if (chatBox.style.display === "none" || chatBox.style.display == "") {
+    chatBox.style.display = "flex";
+  } else {
+    chatBox.style.display = "none";
+  }
+}
