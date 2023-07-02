5261360684801603575b7fc4c192a7099e4f2db4
doquangvnla <138232649+doquangvnla@users.noreply.github.com>
2023-07-02 12:10:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ socket.on('chat message', (msg) => {
 socket.on('DANG_KY_THAT_BAT', () => alert('Vui long chon username khac!'));
 
 function openStream() {
-    const config = { audio: true, video: true };
+    const config = { audio: true, video: false };
     return navigator.mediaDevices.getUserMedia(config);
 }
 
