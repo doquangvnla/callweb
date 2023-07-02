3618957ba61621dd56156794f0b52de2d3ed394f
doquangvnla <138232649+doquangvnla@users.noreply.github.com>
2023-07-02 10:12:55 +0700

    3

--- main.js
+++ main.js
@@ -10,18 +10,17 @@ socket.on('DANH_SACH', arrUser =>{
 	
 	console.log(arrUser);
 	arrUser.forEach(user => {
-        const { ten, peerId } = user;
-        $('#ulUser').append(`<div class="participant-item">
-          <div class="participant-avatar"></div>
-          <div class="participant-name">${ten}</div>
-        </div>`);
-		
-		//them video nguoi den truoc
-		var id = $('#my-peer').html();
-		if(id != peerId){
-			$('#remote_video').append(`<video class="video-frame" id="remoteStream${peerId}" controls></video>`);
-		}
-		
+	     const { ten, peerId } = user;
+            $('#ulUser').append(`<div class="participant-item">
+              <div class="participant-avatar"></div>
+              <div class="participant-name">${ten}</div>
+            </div>`);
+    		
+    		//them video nguoi den truoc
+    		var id = $('#my-peer').html();
+    		if(id != peerId){
+    			$('#remote_video').append(`<video class="video-frame" id="remoteStream${peerId}" controls></video>`);
+    		}
     });
 	
 	socket.on('CO_NGUOI', user =>{
@@ -36,11 +35,14 @@ socket.on('DANH_SACH', arrUser =>{
         $('#remote_video').append(`<video class="video-frame" id="remoteStream${peerId}" controls></video>`);
 		
 		//goi voi nguoi moi vao
-		str
-		.then(stream => {
-			const call = peer.call(peerId, stream);
-			call.on('stream', remoteStream => playStream('remoteStream'+peerId, remoteStream));
-		});
+		
+        str
+    		.then(stream => {
+    		    playStream('localStream', stream);
+    			const call = peer.call(peerId, stream);
+    			call.on('stream', remoteStream => playStream('remoteStream'+peerId, remoteStream));
+    		});
+	
 	});	
 		
 	socket.on('AI_DO_NGAT_KET_NOI', peerId => {
@@ -86,8 +88,11 @@ function playStream(idVideoTag, stream) {
 	if(idVideoTag == 'localStream'){
 		video.muted = true;
 	}
-    video.srcObject = stream;
-    video.play();
+	if(video != undefined && video.srcObject == null){
+	    video.srcObject = stream;
+        video.play();
+	}
+    
 }
 
 //openStream()
@@ -119,13 +124,24 @@ peer.on('open', function(id) {
 	});
   });
   
+  
+//Caller
+$('#btnCall').click(() => {
+    const id = $('#remoteId').val();
+    openStream()
+    .then(stream => {
+        playStream('localStream', stream);
+        const call = peer.call(id, stream);
+        call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+    });
+});
 
 //Callee
 peer.on('call', call => {
     str
     .then(stream => {
         call.answer(stream);
-        playStream('localStream', stream);
+        //playStream('localStream', stream);
         call.on('stream', remoteStream => playStream('remoteStream'+call.peer, remoteStream));
     });
 });
